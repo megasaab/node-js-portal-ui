d2291cade35ae7496f55baef1f80785c56f8d85c
azhg.sabyrzhan@gmail.com <azhg.sabyrzhan@gmali.com>
2021-06-28 21:47:05 +0500

    add header

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import LoginForm from "./components/LoginForm";
 import {Context} from "./index";
 import {useContext, useEffect} from 'react';
 import {observer} from 'mobx-react-lite';
+import Header from "./components/Header";
 
 const App = () => {
     const {store} = useContext(Context);
@@ -25,8 +26,7 @@ const App = () => {
 
     return (
         <div>
-            <h1>{store.isAuth ? `User has logged ${store?.user?.email}` : "Authorization"}</h1>
-            <button onClick={() => store.logout()}>Exit</button>
+            <Header/>
         </div>
     );
 }
